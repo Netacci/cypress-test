c11892cbcb82051599a880ace08a3cfc1dd8f7d0
Chineta Adinnu <chinetaadinnu@gmail.com>
2023-10-09 12:46:23 +0100

    first commit

---
+++ cypress/e2e/countdown.cy.js
@@ -0,0 +1,11 @@
+describe('Test react app', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('checks for vite name', () => {
+    cy.getDataTest('Check vite').contains('Vite + React');
+  });
+  it('clicks button', () => {
+    cy.getDataTest('count button').contains('count').click();
+  });
+});
